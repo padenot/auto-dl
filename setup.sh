55c03b348cb7dd55eeecf80e9c6140365fbc2615
Paul Adenot <paul@paul.cx>
2023-01-07 06:53:25 +0100

    setup uses curl

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
+CURL=`which curl`
+
+[[ -z $CURL ]] && "This needs a curl binary."
+
+$CURL -L -o yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
 chmod +x yt-dlp
